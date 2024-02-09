ed65dd21410637151b35154d77365cf08e4210df
SabaCharmi <saba.charmi@gmail.com>
2024-02-09 13:58:37 -0500

    updated_files

---
+++ prepare_test_dataset/run.py
@@ -0,0 +1,8 @@
+from video_design import video_design
+
+speed = 1
+image_dir = './prepare_test_dataset/stimuli/'
+save_dir = './prepare_test_dataset/videos/'
+
+# run the code to create and save videos
+video_design(speed, image_dir, save_dir)
