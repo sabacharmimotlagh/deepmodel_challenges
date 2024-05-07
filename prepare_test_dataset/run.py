ca4dfc2bc5c069e089ba0c85b934951dde7c5b79
Saba Charmi Motlagh <saba.charmi@gmail.com>
2024-05-07 16:06:14 -0400

    update video generator

--- prepare_test_dataset/run.py
+++ prepare_test_dataset/run.py
@@ -1,8 +1,9 @@
-from video_design import video_design
+from RSVPVideoGenerator import RSVPVideoGenerator
 
-speed = 1
+speed = 5
 image_dir = './prepare_test_dataset/stimuli/'
 save_dir = './prepare_test_dataset/videos/'
 
 # run the code to create and save videos
-video_design(speed, image_dir, save_dir)
+videos = RSVPVideoGenerator(speed, image_dir, save_dir, file_type='tif', patch_size=20, nmasks=10, all_target=False)
+videos.video_design(fps=25)
